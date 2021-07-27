9930b1d316975e3de41e8e134f8d11c28cf8c5fd
Luiz Wynne <luiz@fintechjobs.ie>
2021-07-27 09:51:47 -0300

    Routes and 404 page

---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <p>Projects</p>
+        </div>
+    )
+}
---
+++ src/pages/projects/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Test() {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            <p>Content</p>
+            <p>Content</p>
+        </div>
+    )
+}
