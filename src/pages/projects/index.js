1a9d95c7e8a6e413dc24d5859881f7dd3758e725
Luiz Wynne <luiz@fintechjobs.ie>
2021-07-28 17:18:03 -0300

    Querying projects, stylying and plugins

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,43 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import styles from '../../styles/projects.css'
+import { graphql, Link } from 'gatsby'
 
-export default function Projects() {
+export default function Projects({ data }) {
+
+    const projects = data.allMarkdownRemark.nodes;
+
     return (
         <Layout>
             <div className="portfolio">
-                <h1>Portfolio</h1>
-                <p>Projects</p>
+                <h2>Portfolio</h2>
+                <h3>Projects & Websites I've created</h3>
+                <div className='projects'>
+                    { projects.map(project => (
+                        <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+                            <div>
+                                <h3>{project.frontmatter.title}</h3>
+                                <p>{project.frontmatter.stack}</p>
+                            </div>
+                        </Link>
+                    )) }
+                </div>
             </div>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query ProjectsPage {
+        allMarkdownRemark {
+        nodes {
+            frontmatter {
+            slug
+            stack
+            title
+            }
+            id
+        }
+        }
+    }
+`
